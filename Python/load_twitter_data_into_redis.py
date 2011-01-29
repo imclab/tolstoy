21d1e4ca16bdd795677a79b1fd3efed42c5eeab7
Chris Diehl <cpdiehl@gmail.com>
2011-01-28 16:10:07 -0800

    Moved Ruby files into Ruby subdirectory. Initial commit of Python file to load Twitter data into Redis.

---
+++ Python/load_twitter_data_into_redis.py
@@ -0,0 +1,27 @@
+import json
+import redis
+
+# connect to the database
+rdb = redis.Redis(host='localhost', port=6379, db=1)
+rdb.delete('tweets')
+
+# open the data file containing the json dump
+f = open('earthquake','r')
+
+# read in all of the lines
+tweet_strings = f.readlines()
+
+i = 0
+for tweet_string in tweet_strings:
+
+    # push tweet into the tweets hash table
+    try:
+        tweet = json.loads(tweet_string)
+    except:
+        print "Problematic tweet: %s" % tweet_string
+    else:
+        rdb.hset('tweets',str(tweet['id']),json.dumps(tweet))
+    
+    i += 1
+    if i % 500 == 0:
+        print "%d tweets processed." % i
