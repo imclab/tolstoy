67304e7994164099d209f5ba42e51e55afdb72b1
Chris Blow <unthinkingly@gmail.com>
2011-01-28 16:41:17 -0800

    redis support

--- tolstoy.rb
+++ tolstoy.rb
@@ -1,17 +1,14 @@
 require 'sinatra'
+require 'redis'
 
-configure do
-  require 'redis'
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-end
 
 # access redis within the context of an HTTP request
 get '/' do
-  "Hello!"
-end
-
-get 'database_test' do
-  @foos = redis.lrange("foos", 0, -1) # Array
-  @foos.inspect
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  if redis 
+    "Redis appears to be working ..."
+  else
+    "Redis is down"
+  end
 end
