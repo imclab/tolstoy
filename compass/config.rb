dd199ae27e6be7d7d1878b66a75b8f420582dd2f
Chris Blow <unthinkingly@gmail.com>
2011-02-25 17:19:00 -0800

    straighten out compass

--- compass/config.rb
+++ compass/config.rb
@@ -3,9 +3,9 @@ require 'html5-boilerplate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "src"
-images_dir = "images"
-javascripts_dir = "js"
+css_dir = "../public/css"
+sass_dir = "../public/src"
+images_dir = "../public/images"
+javascripts_dir = "../public/js"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
--- tolstoy.rb
+++ tolstoy.rb
@@ -2,14 +2,9 @@ require 'rubygems'
 require 'sinatra'
 require 'redis'
 
-if ENV['RACK_ENV'] == 'production'
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  REDIS= Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-else
-  REDIS= Redis.new(:host => "localhost", :port => "6379")
-end
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS= Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 get '/' do
-  REDIS['foo'] = 'bar'
-  "Redis appears to be working ..."
+  haml :index
 end
